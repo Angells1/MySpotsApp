cc4fb788b4cc7033519a23a89d456d8889faa69b
Gabriel Angelo <gabriel.angelsad1@gmail.com>
2020-07-02 23:57:48 -0300

    make changes on newspot page

--- web/src/Components/NewSpot/NewSpot.js
+++ web/src/Components/NewSpot/NewSpot.js
@@ -5,7 +5,13 @@ import NavBar from '../NavBar/NavBar'
 
 const NewSpot = () => {
     
+    //nome do anuncio
+    //descrição
+    //preço
+    //Tipo
+    //fotos
 
+    
     return(
         <>
         <NavBar/> 
@@ -15,7 +21,11 @@ const NewSpot = () => {
                         
                     <form className="form-cad">
 
-                        <input type="text" placeholder="Nome do Anuncio" className="input-global"/>
+                        <input type="text" placeholder="Titulo do Anuncio" className="input-global"/>
+
+                        <label className="lbl-global" htmlFor="description">Descrição</label>
+
+                        <textarea class="desc-textarea" name="description" id="" cols="40" rows="10"></textarea>
 
                     </form>
 
