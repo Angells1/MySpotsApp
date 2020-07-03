8d8694954a8550e20a0b3d1c7811ebb064b0da95
Gabriel Angelo <gabriel.angelsad1@gmail.com>
2020-07-03 01:23:05 -0300

    more changes on newspot page

--- web/src/Components/NewSpot/NewSpot.js
+++ web/src/Components/NewSpot/NewSpot.js
@@ -21,11 +21,27 @@ const NewSpot = () => {
                         
                     <form className="form-cad">
 
-                        <input type="text" placeholder="Titulo do Anuncio" className="input-global"/>
+                    <label className="lbl-global" htmlFor="title">Titulo do Anúncio</label>
+                        <input type="text" name="title" placeholder="Ex: Ps4 usado" className="input-global"/>
 
                         <label className="lbl-global" htmlFor="description">Descrição</label>
 
-                        <textarea class="desc-textarea" name="description" id="" cols="40" rows="10"></textarea>
+                        <textarea class="desc-textarea" placeholder="Ex: Ps4 usado 500gb, com caixa." name="description" id="" cols="40" rows="10"></textarea>
+                        
+                        <label className="lbl-global" htmlFor="price">Preço do Anúncio</label>
+
+                        <input type="number" name="price" placeholder="Ex: R$: 1500,00" className="input-global"/>
+
+                        <label className="lbl-global" htmlFor="price">Categoria do Produto</label>
+
+                        <select class="select-global" name="" id="">
+                            <option value="">Slas</option>
+
+                        </select>
+
+                        <label className="lbl-global" htmlFor="photos">Fotos do Produto</label>
+                        <input type="file" name="photos" id=""/>
+                        
 
                     </form>
 
